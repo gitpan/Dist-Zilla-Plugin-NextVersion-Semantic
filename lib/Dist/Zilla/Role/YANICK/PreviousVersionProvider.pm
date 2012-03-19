0d1e736a4f21cbb625942541185a640d2697c155
Yanick Champoux <yanick+cpan@babyl.dyndns.org>
2012-03-19 23:11:21 +0000

    Import of YANICK/Dist-Zilla-Plugin-NextVersion-Semantic-0.1.0 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-NextVersion-Semantic
    gitpan-cpan-version:      0.1.0
    gitpan-cpan-path:         YANICK/Dist-Zilla-Plugin-NextVersion-Semantic-0.1.0.tar.gz
    gitpan-cpan-author:       YANICK
    gitpan-cpan-maturity:     released

---
+++ lib/Dist/Zilla/Role/YANICK/PreviousVersionProvider.pm
@@ -0,0 +1,60 @@
+package Dist::Zilla::Role::YANICK::PreviousVersionProvider;
+BEGIN {
+  $Dist::Zilla::Role::YANICK::PreviousVersionProvider::AUTHORITY = 'cpan:YANICK';
+}
+{
+  $Dist::Zilla::Role::YANICK::PreviousVersionProvider::VERSION = '0.1.0';
+}
+# provides the previously released version
+
+
+use strict;
+use warnings;
+
+use Moose::Role;
+
+requires 'provide_previous_version';
+
+
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Dist::Zilla::Role::YANICK::PreviousVersionProvider
+
+=head1 VERSION
+
+version 0.1.0
+
+=head1 DESCRIPTION
+
+Role for L<Dist::Zilla::Plugin> classes that return
+the previously released version.
+
+The namespace contains I<YANICK> simply because I didn't want
+to encroach on the official namespace without asking permission.
+If allowed, this role will migrate to
+I<Dist::Zilla::Role::PreviousVersionProvider>.
+
+=head1 METHODS REQUIRED BY THE ROLE
+
+=head2 provide_previous_version
+
+Returns the previously released version
+
+=head1 AUTHOR
+
+Yanick Champoux <yanick@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Yanick Champoux.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
